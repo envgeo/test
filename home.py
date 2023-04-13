3cd0ff9caafcf6c4de0cbf7f99f0c019e0809e71
envgeo <130520553+envgeo@users.noreply.github.com>
2023-04-13 14:06:16 +0900

    Add files via upload

---
+++ home.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+
+@st.cache_resource
+def main():
+    # タイトル
+    st.title('SEAWATER DATA JAPAN (b01)')
+    # サブレベルヘッダ
+    st.subheader('select sub menu')
+    # ヘッダ
+    # st.header('by TOYOHO ISHIMURA')
+    # 純粋なテキスト
+    st.text('original data: xxx in prep.')
+    # 純粋なテキスト
+    st.text('visualized by TOYOHO ISHIMURA (Python with Streamlit)')
+
+    # # マークダウンテキスト
+    # st.markdown('**Markdown is available **')
+    # # LaTeX テキスト
+    # st.latex(r'\bar{X} = \frac{1}{N} \sum_{n=1}^{N} x_i')
+    # # コードスニペット
+    # st.code('print(\'Hello, World!\')')
+    # # エラーメッセージ
+    # st.error('in case of error: push reload button or reload this site')
+    # # 警告メッセージ
+    # st.warning('Warning message')
+    # # 情報メッセージ
+    # st.info('Information message')
+    # # 成功メッセージ
+    # st.success('Success message')
+    # # 例外の出力
+    # st.exception(Exception('Oops!'))
+    # # 辞書の出力
+    # d = {
+    #     'foo': 'bar',
+    #     'users': [
+    #         'alice',
+    #         'bob',
+    #     ],
+    # }
+    # st.json(d)
+
+    # st.write('in case of error: push reload button or reload this site')
+
+    video_file = open('data/d18O_all.mp4', 'rb')
+    video_bytes = video_file.read()
+
+    st.video(video_bytes)
+    
+    st.write('movie output by GMT')
+    
+if __name__ == '__main__':
+    main()
